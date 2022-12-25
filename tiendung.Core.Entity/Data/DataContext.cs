27a5e13c5fba201cfc1a863b1a6541b235e3ce8c
tiendung <tiendung.gg9@gmail.com>
2022-12-25 12:49:33 +0700

    ver 1.3 update

--- tiendung.Core.Entity/Data/DataContext.cs
+++ tiendung.Core.Entity/Data/DataContext.cs
@@ -4,6 +4,8 @@ namespace tiendung.Core.Entity.Data
 {
     public class DataContext : DbContext
     {
-
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
--- tiendung.Core.Entity/Model/BaseModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace tiendung.Core.Entity.Model
-{
-    public class BaseModel
-    {
-        public Guid Id { get; set; }
-    }
-}
